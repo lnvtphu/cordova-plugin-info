39bb9b306c7049bdffeb57d6b5982aebbebfe41f
Phu Le <lnvtphu@gmail.com>
2018-08-23 15:48:50 +0700

    Update the code for window

--- src/windows/InfoProxy.js
+++ src/windows/InfoProxy.js
@@ -4,17 +4,29 @@ module.exports = {
             var packageSpecificToken;
 
             packageSpecificToken = Windows.System.Profile.HardwareIdentification.getPackageSpecificToken(null);
-    
+
             // hardware id, signature, certificate IBuffer objects 
             // that can be accessed through properties.
             var hardwareId = packageSpecificToken.id;
-            var signature = packageSpecificToken.signature;
-            var certificate = packageSpecificToken.certificate;
-            console.log(Windows.System.Profile);
-            console.log(Windows.System.Devices);
             var deviceId = Windows.Storage.Streams.DataReader.fromBuffer(hardwareId).readGuid();
-            
-            successCallback(deviceId);
+
+            // window version
+            var deviceFamilyVersion = Windows.System.Profile.AnalyticsInfo.versionInfo.deviceFamilyVersion;
+            var deviceFamilyVersionUlong = parseInt(deviceFamilyVersion).toString(16),
+                bit00 = parseInt(deviceFamilyVersionUlong.slice(deviceFamilyVersionUlong.length - 4), 16),
+                bit16 = parseInt(deviceFamilyVersionUlong.slice(deviceFamilyVersionUlong.length - 8, deviceFamilyVersionUlong.length - 4), 16),
+                bit32 = parseInt(deviceFamilyVersionUlong.slice(deviceFamilyVersionUlong.length - 12, deviceFamilyVersionUlong.length - 8), 16),
+                bit48 = parseInt(deviceFamilyVersionUlong.slice(0, deviceFamilyVersionUlong.length - 12), 16);
+            var version = [bit48, bit32, bit16, bit00].join(".");
+            //
+            var easClientDeviceInformation = Windows.Security.ExchangeActiveSyncProvisioning.EasClientDeviceInformation();
+            var infoDevice = {
+                id: easClientDeviceInformation.id,
+                productName: easClientDeviceInformation.systemProductName,
+                version: version,
+                manufacture: easClientDeviceInformation.systemManufacturer
+            };
+            successCallback(infoDevice);
         } catch (error) {
             errorCallback(error);
         }
